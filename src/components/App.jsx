8d5632c7d35d0b4e49e1be2c8f7bb28cee1790da
The Advocate <134937174+lawyerWho@users.noreply.github.com>
2023-05-31 22:34:49 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      // if (name === "fName") {
+      //   return {
+      //     fName: value,
+      //     lName: prevValue.lName,
+      //     email: prevValue.email
+      //   };
+      // } else if (name === "lName") {
+      //   return {
+      //     fName: prevValue.fName,
+      //     lName: value,
+      //     email: prevValue.email
+      //   };
+      // } else if (name === "email") {
+      //   return {
+      //     fName: prevValue.fName,
+      //     lName: prevValue.lName,
+      //     email: value
+      //   };
+      // }
+
+      // Spread Operator
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  //Spread Operator
+  //   setContact((prevValue) => ({ ...prevValue, [name]: value }));
+  // }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Spread Operator
+// const citrus = ["lemon", "orange", "pineapple"];
+// const fruits = ["mango", "banana", "plum", ...citrus];
+
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "John",
+//   lName: "Doe"
+// };
+
+// const users = {
+//   ...fullName,
+//   id: 1,
+//   username: "Nameless"
+// };
+
+// console.log(users);
